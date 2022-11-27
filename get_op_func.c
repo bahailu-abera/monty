30d4cf98c1118c19f8a1da54057846bcf596434e
bahailu <aberabahailu4@gmail.com>
2022-11-27 22:39:16 +0300

    helper function

---
+++ get_op_func.c
@@ -0,0 +1,30 @@
+#include "monty.h"
+
+/**
+ * get_op_func - selects the correct function for the operation
+ * @stack: the stack data structure
+ * @line_number: the current reading line number in the input file
+ *
+ * Return: the selected function
+ */
+void  (*get_op_func(stack_t *stack,
+		    unsigned int line_number))(stack_t **stack,
+					       unsigned int line_number)
+{
+	unsigned int i;
+	instruction_t oper[] = {
+		{"push", push},
+		{"pall", pall},
+		{NULL, NULL}
+	};
+
+	for (i = 0; oper[i].opcode != NULL; i++)
+	{
+		if (strcmp(oper[i].opcode, stream.token) == 0)
+		{	return (oper[i].f);
+		}
+	}
+	fprintf(stderr, "L%d: Unknown instruction %s\n", line_number, stream.token);
+	free_stack(stack);
+	exit(EXIT_FAILURE);
+}
