0760593e56fa387092091c3771bcd2f6f14b8cf0
bahailu <aberabahailu4@gmail.com>
2022-11-27 23:49:54 +0300

    dont pop

--- pint.c
+++ pint.c
@@ -9,16 +9,11 @@
  */
 void pint(stack_t **stack, unsigned int line_number)
 {
-	stack_t *node;
-
 	if (stack == NULL || *stack == NULL)
 	{
 		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	node = *stack;
-	(*stack) = (*stack)->next;
-	printf("%d\n", node->n);
-	free(node);
-	node = NULL;
+
+	printf("%d\n", (*stack)->n);
 }
