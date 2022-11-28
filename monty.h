728e81062b0fd393009c603a44f724e2a569c743
bahailu <aberabahailu4@gmail.com>
2022-11-28 10:32:17 +0300

    header file updated

--- monty.h
+++ monty.h
@@ -59,6 +59,14 @@ void pint(stack_t **stack, unsigned int line_number);
 void pop(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
+void sub(stack_t **stack, unsigned int line_number);
+void mul(stack_t **stack, unsigned int line_number);
+void _div(stack_t **stack, unsigned int line_number);
+void mod(stack_t **stack, unsigned int line_number);
+void free_stack(stack_t *stack);
+void free_stack(stack_t *stack);
+void free_stack(stack_t *stack);
+void free_stack(stack_t *stack);
 void free_stack(stack_t *stack);
 extern stream_t stream;
 
