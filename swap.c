482f4cfcb8706e0bd4a31e22ec04c3535b75fd10
bahailu <aberabahailu4@gmail.com>
2022-11-28 09:47:08 +0300

    error msg

--- swap.c
+++ swap.c
@@ -14,7 +14,7 @@ void swap(stack_t **stack, unsigned int line_number)
 
 	if (node == NULL || node->next == NULL)
 	{
-		fprintf(stderr, "L%d: can't swap, stack toop short\n", line_number);
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	n = node->n;
